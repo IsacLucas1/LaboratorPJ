2d42823e397dc233c220daca4606d2b38271973e
IsacLucas1 <lucas.isac@student.upt.ro>
2025-01-13 01:27:11 +0200

    Add files via upload

---
+++ TemaLaborator9JPA/TemaLaborator9JPA/src/main/java/com/example/TemaLaborator9JPA/Masina.java
@@ -0,0 +1,69 @@
+package com.example.TemaLaborator9JPA;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="masini2")
+public class Masina {
+
+    @Id
+    @Column(name = "nr_inmatriculare")
+    private String nr_inmatriculare;
+    private String marca;
+    private int anul;
+    private String culoare;
+    private int numar_km;
+
+    public Masina(){}
+    public Masina(String nr_inmatriculare, String marca, int anul, String culoare, int numar_km){
+        this.nr_inmatriculare = nr_inmatriculare;
+        this.marca = marca;
+        this.anul = anul;
+        this.culoare = culoare;
+        this.numar_km = numar_km;
+    }
+
+    public String getNr_inmatriculare() {
+        return nr_inmatriculare;
+    }
+    public void setNr_inmatriculare(String nr_inmatriculare) {
+        this.nr_inmatriculare = nr_inmatriculare;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getAnul() {
+        return anul;
+    }
+    public void setAnul(int anul) {
+        this.anul = anul;
+    }
+
+    public String getCuloare() {
+        return culoare;
+    }
+    public void setCuloare(String culoare) {
+        this.culoare = culoare;
+    }
+
+    public int getNumar_km() {
+        return numar_km;
+    }
+    public void setNumar_km(int numar_km) {
+        this.numar_km = numar_km;
+    }
+
+    @Override
+    public String toString() {
+        return "Numar inmatriculare: " + nr_inmatriculare+", Marca: " + marca + ", An fabricatie: " + anul + ", Culoare: " + culoare + ", Nr Km: " + numar_km;
+    }
+}
+
