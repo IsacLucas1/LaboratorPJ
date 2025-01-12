cb4e20c20524e5ca51ddf3683e9cffcc7aab5722
IsacLucas1 <lucas.isac@student.upt.ro>
2025-01-13 00:44:48 +0200

    Add files via upload

---
+++ TemaLaborator9SDJDBC/src/test/java/com/example/TemaLaborator9SDJDBC/TemaLaborator9SdjdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TemaLaborator9SDJDBC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemaLaborator9SdjdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
